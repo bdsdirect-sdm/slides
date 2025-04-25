39e72da554c03788d40d19a6a6a4d535a5f6002b
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2025-04-26 05:27:07 +0530

    first commit

---
+++ src/Slides.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Slides.css';
+
+function Slides() {
+    return (
+        <div class="main-div">
+
+            <div class="inner-div">
+                <div class="slides">1</div>
+                <div class="slides">2</div>
+                <div class="slides">1</div>
+                <div class="slides">2</div>
+            </div>
+
+            <div class="inner-div">
+                <div class="slides">3</div>
+                <div class="slides">4</div>
+                <div class="slides">3</div>
+                <div class="slides">4</div>
+            </div>
+
+            <div class="inner-div">
+                <div class="slides">5</div>
+                <div class="slides">6</div>
+                <div class="slides">5</div>
+                <div class="slides">6</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Slides
