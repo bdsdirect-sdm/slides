8c381bd5ac49f75f6df4f889afd447c0dd8539ac
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2025-05-02 02:56:45 +0530

    thankyou page added

--- src/Thankyou.js
+++ src/Thankyou.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './Thankyou.css';
+import React from 'react';
+import './ThankYouPage.css'; // Make sure to create this CSS file
+import { useNavigate } from 'react-router-dom';
 
-function Thankyou() {
+const ThankYouPage = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1 className='thankyou-heading'>Thank You</h1>
+        <div className="thankyou-container">
+            <div className="thankyou-card">
+                <h1>Thank you for your purchase.</h1>
+                <p>
+                    We'll call you in 30 minutes, but you can check new post in our dashboard section—{' '}
+                    <span onClick={() => { navigate("/map") }}>
+                        dashboard
+                    </span>
+                </p>
+                <button onClick={() => { navigate("/map") }}>
+                    Back to Home
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Thankyou
+export default ThankYouPage;
